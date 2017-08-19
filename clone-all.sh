5cd0cee5abaa51f61191930b4b5ad8d456a256d1
Gorka <gorka@aquigorka.com>
2017-08-19 14:01:49 +0200

    Fixes typo

--- clone-all.sh
+++ clone-all.sh
@@ -1,2 +1,1 @@
-USER=AquiGorka; curl -k "https://api.github.com/users/$USER/repos?per_page=100"
-| grep -o 'https://github.com/$USER/[^"]*.git' | xargs -L1 git clone
+USER=AquiGorka; curl -k "https://api.github.com/users/$USER/repos?per_page=100" | grep -o "https://github.com/$USER/[^\"]*.git" | xargs -L1 git clone
